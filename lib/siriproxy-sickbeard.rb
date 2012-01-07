d1222b7d5d65a5da79fff71aebe9fd4af0a35717
Shawn Conroyd <mongo527@gmail.com>
2012-01-06 23:48:46 -0500

    Cleaned up a little

--- lib/siriproxy-sickbeard.rb
+++ lib/siriproxy-sickbeard.rb
@@ -15,7 +15,7 @@ class SiriProxy::Plugin::SickBeard < SiriProxy::Plugin
         @api_key = config["sickbeard_api"]
     end
     
-    api_url ="http://"+@host+":"+@port+"/api/"+@api_key+"/?cmd="
+    api_url ="http://#{@host}:#{@port}/api/#{@api_key}/?cmd="
     
     listen_for /force sickbeard backlog/i do
         open(api_url + "sb.forcesearch") do |f|
