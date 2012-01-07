82d67fee86b40faa5b6651a048bb141b4ef53f61
Shawn Conroyd <mongo527@gmail.com>
2012-01-06 21:21:23 -0500

    Added 2 commands for Siri

--- lib/siriproxy-nba.rb
+++ lib/siriproxy-nba.rb
@@ -1,7 +1,7 @@
 require 'cora'
 require 'siri_objects'
 require 'open-uri'
-#require 'nokogiri'
+require 'regexp'
 
 #############
 # This is a plugin for SiriProxy that will allow you to control SickBeard.
@@ -18,5 +18,44 @@ class SiriProxy::Plugin::SickBeard < SiriProxy::Plugin
         @password = config["sickbeard_password"]
     end
     
+    @api_url = "http://" + @host + ":" + @port + "/api/" + @api_key + "/?cmd="
     
+    
+    
+    listen_for /force sickbeard backlog/i do
+        open(@api_url + "sb.forcesearch") do |f|
+            no = 1
+            f.each do |line|
+                if /result.*success/.match("#{line}")
+                    say "SickBeard is refreshing the Backlog."
+                else
+                    say "There was a problem refreshing the Backlog."
+                end
+            end
+        end
+    end
+
+    listen_for /add new show/i do
+        showName = ask "What Show would you like to add?"
+        showID = ""
+        open (@api_url + "sb.searchtvdb&name=" + showName) do |f|
+            no =1
+            f.each do |line|
+                if /tvdbid/.match("#{line}")
+                    showID = (/[0-9].*/.match("#{line}")).to_s()
+                    say showName + " will be added to SickBeard."
+                end
+            end
+        end
+        open (@api_url + "show.addnew&tvdbid=" + showID) do |f|
+            no = 1
+            f.each do |line|
+                if /result.*success/.match("#{line}")
+                    say showName + " has been added to SickBeard."
+                else
+                    say "There was a problem adding " + showName + " to SickBeard."
+                end
+            end
+        end
+    end
 end
