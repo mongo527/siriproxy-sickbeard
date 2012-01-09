62fd6b463d7444d8638a2b1ecf2cbe0e947c3dc9
Shawn Conroyd <mongo527@gmail.com>
2012-01-08 23:51:50 -0500

    Testing again... Not working state... sorry

--- lib/siriproxy-sickbeard.rb
+++ lib/siriproxy-sickbeard.rb
@@ -168,7 +168,7 @@ class SiriProxy::Plugin::SickBeard < SiriProxy::Plugin
             open ("http://#{@host}:#{@port}/api/#{@api_key}/?cmd=sb.searchtvdb&name=#{showName}") do |f|
                 f.each do |line|
                     if /name/.match("#{line}")
-                        nameLine = "#{line}".gsub(/name:""*\\*\,*/, "").strip
+                        nameLine = "#{line}".gsub(/""*\\*name:\,*/, "").strip
                         showNameList.push(nameLine)
                         count += 1
                     end
